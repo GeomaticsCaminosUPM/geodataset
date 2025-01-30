3a323bc69fb0e4f91a34c59a4c6705d69e83b059
GeomaticsCaminosUPM <geomatics.caminosupm@gmail.com>
2025-01-30 16:40:03 +0100

    Update __init__.py

--- geodataset/__init__.py
+++ geodataset/__init__.py
@@ -1,6 +1,6 @@
 from .geodataset import GeoDataset 
 from .grid import Grid
-from .image import ImageDataset
+from .image import ImageData
 from .segmentation import SegmentationData
 
 __version__ = '0.1.0'
