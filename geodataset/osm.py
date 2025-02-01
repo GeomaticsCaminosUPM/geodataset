8cbc58407af68ea9e851847c0b1f359005c868e8
GeomaticsCaminosUPM <geomatics.caminosupm@gmail.com>
2025-02-01 11:21:21 +0100

    Update osm.py

--- geodataset/osm.py
+++ geodataset/osm.py
@@ -6,10 +6,14 @@ def overpass_api_query(query:str,bounds:gpd.GeoDataFrame|gpd.GeoSeries):
 
     bbox = bounds.to_crs(4326).total_bounds
     bbox = f"{bbox[1]},{bbox[0]},{bbox[3]},{bbox[2]}"
+    query = query.replace("{{bbox}}",bbox)
     query = query.replace("{bbox}",bbox)
     query = query.replace("[out:xml]","[out:json]")
     overpass_url = 'https://overpass-api.de/api/interpreter'
     response = requests.get(overpass_url, params={'data': query})
+    if response.status_code != 200:
+        raise Exception(f"Overpass turbo api query failed with statud code {response.status_code}. The failed query was: {query}")
+        
     geojson_response = json2geojson(response.json())
     gdf = gpd.GeoDataFrame.from_features(geojson_response,crs=4326).reset_index(drop=True)
     new_gdf = gdf['tags'].apply(pd.Series)
